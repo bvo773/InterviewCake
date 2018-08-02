75881938735d3472172cf49b70c5b9ebc1e9f2e0
Nate Lee <>
2018-08-01 22:58:46 -0500

    Add s04p01

---
+++ InterviewCake/src/com/lee/section04/Problem01.java
@@ -0,0 +1,26 @@
+package com.lee.section04;
+
+public class Problem01 {
+    int FindRotationPoint(String[] words) {
+         int floorIndex, midIndex, ceilingIndex;
+         floorIndex = 0;
+         ceilingIndex = words.length-1;
+
+         String firstWord = words[0];
+
+         while (floorIndex < ceilingIndex) {
+             midIndex = (floorIndex + ceilingIndex) / 2;
+             String middleWord = words[midIndex];
+             // Compare firstword with midIndex;
+             if (middleWord.compareTo(firstWord) == -1) {
+                 ceilingIndex = midIndex;
+                 continue;
+             } else {
+                 floorIndex = midIndex;
+                 continue;
+             }
+         }
+
+         return floorIndex;
+    }
+}
